75c9bccbed0f31eeef873377f31436e53cb5c623
dkstjdwns114 <sungjoon256@gmail.com>
2021-02-26 23:36:56 +0900

    Updates

---
+++ precache-manifest.080036403649df5709436f6a369a1dd4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62cfae98db2054dada72b99c3bae2daf",
+    "url": "/PortFolio/index.html"
+  },
+  {
+    "revision": "f45e9b4837d3041c65ce",
+    "url": "/PortFolio/static/css/main.04f926a0.chunk.css"
+  },
+  {
+    "revision": "3ebecd081e1239eb89b1",
+    "url": "/PortFolio/static/js/2.b678134d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PortFolio/static/js/2.b678134d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f45e9b4837d3041c65ce",
+    "url": "/PortFolio/static/js/main.c4686a1e.chunk.js"
+  },
+  {
+    "revision": "fdd5dfb3fec5b90bae3c",
+    "url": "/PortFolio/static/js/runtime-main.35884d7a.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/PortFolio/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "26a3b52821fbbcf6cd9c1319b23828bd",
+    "url": "/PortFolio/static/media/your photo.26a3b528.png"
+  }
+]);
